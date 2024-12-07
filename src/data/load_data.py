d1dd66e140e34f0e247e72a85eb38591a24ed391
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-07 12:09:26 -0500

    Create load_data.py

---
+++ src/data/load_data.py
@@ -0,0 +1,19 @@
+%run imports.py
+
+# Convert excel file into DataFrame
+# If you have trouble reading this file, you may need to do this command in the terminal: pip install openpyxl 
+autism = pd.read_excel('../../data/Autism_Screening_Adult.xlsx')
+autism = autism.drop(columns=['Ethnicity','Country_of_Res','Used_App_Before','Relation','Age_Desc'])
+
+# Convert "yes" values to 1 and "no" values to 0
+autism = autism.replace({'no':0,'yes':1,'NO':0,'YES':1})
+
+# Convert "f" to 0 and "m" to 1
+autism = autism.replace({'f':0,'m':1})
+
+# Convert '?' and the outlier to NaN
+autism = autism.replace('?',np.nan)
+autism = autism.replace(383.0,np.nan)
+
+# Drop missing values and outlier
+autism = autism.dropna()
