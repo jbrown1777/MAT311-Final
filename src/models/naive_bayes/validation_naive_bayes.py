76d8656ed6af46d4c9c460e2247f6e10402531d8
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:30:42 -0500

    Update validation_naive_bayes.py

--- src/models/naive_bayes/validation_naive_bayes.py
+++ src/models/naive_bayes/validation_naive_bayes.py
@@ -1,5 +1,8 @@
-# Print values of the confusion matrix
-printConfusionMatrix(y_val, y_pred_val_bayes, 'Gaussian Naive Bayes', 'Validation')
+from src.data.helper_functions import printConfusionMatrix,printResults
 
-# Print results
-printResults(y_val, y_pred_val_bayes)
+def validate_naive_bayes(y_val,y_pred_val_bayes):
+    # Print values of the confusion matrix
+    printConfusionMatrix(y_val, y_pred_val_bayes, 'Gaussian Naive Bayes', 'Validation')
+
+    # Print results
+    printResults(y_val, y_pred_val_bayes,'Gaussian Naive Bayes')
