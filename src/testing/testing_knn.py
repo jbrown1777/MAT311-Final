904cbde027f82a7376b1eec043a4fa0a7c46a845
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:33:08 -0500

    Update testing_knn.py

--- src/testing/testing_knn.py
+++ src/testing/testing_knn.py
@@ -1,8 +1,11 @@
-# KNN was the best model
-y_pred_test_knn = knn.predict(X_test)
+from src.data.helper_functions import *
 
-# Print values of the confusion matrix
-printConfusionMatrix(y_test, y_pred_test_knn, 'KNN', 'Test')
+def testing_knn(knn, X_test, y_test):
+    # KNN was the best model
+    y_pred_test_knn = knn.predict(X_test)
 
-# Print results
-printResults(y_test, y_pred_test_knn)
+    # Print values of the confusion matrix
+    printConfusionMatrix(y_test, y_pred_test_knn, 'KNN', 'Test')
+
+    # Print results
+    printResults(y_test, y_pred_test_knn)
