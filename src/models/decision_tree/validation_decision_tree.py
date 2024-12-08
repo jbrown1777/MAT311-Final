d453096c3ea68e5cc61136300a28455a60d8a986
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:29:45 -0500

    Update validation_decision_tree.py

--- src/models/decision_tree/validation_decision_tree.py
+++ src/models/decision_tree/validation_decision_tree.py
@@ -1,12 +1,17 @@
-# Print values of the confusion matrix
-printConfusionMatrix(y_val, y_pred_val_decision_tree, 'Decision Tree', 'Validation')
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from src.data.helper_functions import *
 
-# Print results
-printResults(y_val, y_pred_val_decision_tree)
-print()
+def validate_tree(decision_tree, y_val, y_pred_val_decision_tree,features,depth_limit):
+    # Print values of the confusion matrix
+    printConfusionMatrix(y_val, y_pred_val_decision_tree, 'Decision Tree', 'Validation')
 
-# Print the tree
-plt.figure(figsize=(4,4))
-plot_tree(decision_tree, feature_names=features, class_names=['No ASD','Yes ASD'], filled=True)
-plt.title(f'Decision Tree (Features: {features}, Max Depth: {depth_limit})')
-plt.show()
+    # Print results
+    printResults(y_val, y_pred_val_decision_tree,'Decision Tree')
+    print()
+
+    # Print the tree
+    plt.figure(figsize=(4,4))
+    plot_tree(decision_tree, feature_names=features, class_names=['No ASD','Yes ASD'], filled=True)
+    plt.title(f'Decision Tree (Features: {features}, Max Depth: {depth_limit})')
+    plt.show()
