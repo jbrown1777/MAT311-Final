4878dcb9df5464bc22e466951036beda2d44e9c8
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:30:31 -0500

    Update training_naive_bayes.py

--- src/models/naive_bayes/training_naive_bayes.py
+++ src/models/naive_bayes/training_naive_bayes.py
@@ -1,3 +1,8 @@
-naive_bayes = GaussianNB()
-naive_bayes.fit(X_train, y_train)
-y_pred_val_bayes = naive_bayes.predict(X_val)
+from sklearn.naive_bayes import GaussianNB
+
+def training_naive_bayes(X_train,y_train,X_val):
+    naive_bayes = GaussianNB()
+    naive_bayes.fit(X_train, y_train)
+    y_pred_val_bayes = naive_bayes.predict(X_val)
+
+    return naive_bayes, y_pred_val_bayes
