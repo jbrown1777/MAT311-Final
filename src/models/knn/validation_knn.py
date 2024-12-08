4197900dc575ca9a4a528c57fb69986fcbafb710
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:32:00 -0500

    Update validation_knn.py

--- src/models/knn/validation_knn.py
+++ src/models/knn/validation_knn.py
@@ -1,12 +1,18 @@
-# Generate new model with the best k-value
-knn = KNeighborsClassifier(n_neighbors=best_k)
+from sklearn.neighbors import KNeighborsClassifier
+from src.data.helper_functions import printConfusionMatrix,printResults
 
-# Validate the new model
-knn.fit(X_train, y_train)
-y_pred_val_knn = knn.predict(X_val)
+def validate_knn(best_k,X_train,y_train,X_val,y_val):
+    # Generate new model with the best k-value
+    knn = KNeighborsClassifier(n_neighbors=best_k)
 
-# Print values of the confusion matrix
-printConfusionMatrix(y_val, y_pred_val_knn, 'KNN', 'Validation')
+    # Validate the new model
+    knn.fit(X_train, y_train)
+    y_pred_val_knn = knn.predict(X_val)
 
-# Print results
-printResults(y_val, y_pred_val_knn)
+    # Print values of the confusion matrix
+    printConfusionMatrix(y_val, y_pred_val_knn, 'KNN', 'Validation')
+
+    # Print results
+    printResults(y_val, y_pred_val_knn,'KNN')
+
+    return knn, y_pred_val_knn
