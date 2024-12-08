30a2653107d387d3d1947f8d91086a3d3fd6e376
jbrown1777 <159035023+jbrown1777@users.noreply.github.com>
2024-12-08 15:31:49 -0500

    Update training_knn.py

--- src/models/knn/training_knn.py
+++ src/models/knn/training_knn.py
@@ -1,11 +1,15 @@
-best_score = 0
-best_k = 0
+from sklearn.neighbors import KNeighborsClassifier
 
-for k in range(1,21):
-    knn = KNeighborsClassifier(n_neighbors=k)
-    knn.fit(X_train, y_train)
-    #y_pred_val = knn.predict(X_val)
-    score = knn.score(X_val, y_val)
-    if score > best_score:
-        best_score = score
-        best_k = k
+def training_knn(X_train,y_train,X_val,y_val):
+    best_score = 0
+    best_k = 0
+    
+    for k in range(1,21):
+        knn = KNeighborsClassifier(n_neighbors=k)
+        knn.fit(X_train, y_train)
+        score = knn.score(X_val, y_val)
+        if score > best_score:
+            best_score = score
+            best_k = k
+
+    return best_k
